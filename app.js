245f7f1d33903e843bbc856ee177ad2d60e45e13
Ricardo Dias <slowmotion@outlook.com>
2018-01-10 19:04:10 +0000

    User model and new user route - still video 11

--- app.js
+++ app.js
@@ -8,29 +8,35 @@ mongoose.Promise = global.Promise;
 
 const producRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
+const usersRoutes = require("./api/routes/user");
 
-mongoose.connect(`mongodb://slow-shop:${process.env.MONGO_ATLAS_PW}@node-rest-shop-shard-00-00-y3dr8.mongodb.net:27017,node-rest-shop-shard-00-01-y3dr8.mongodb.net:27017,node-rest-shop-shard-00-02-y3dr8.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin`);
-
+mongoose.connect(
+  `mongodb://slow-shop:${
+    process.env.MONGO_ATLAS_PW
+  }@node-rest-shop-shard-00-00-y3dr8.mongodb.net:27017,node-rest-shop-shard-00-01-y3dr8.mongodb.net:27017,node-rest-shop-shard-00-02-y3dr8.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin`
+);
 
 app.use(morgan("dev"));
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Accedd-Control-Allow-Origin', '*');
-  res.header('Accedd-Control-Allow-Headers', 
-    'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Accedd-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+  res.header("Accedd-Control-Allow-Origin", "*");
+  res.header(
+    "Accedd-Control-Allow-Headers",
+    "Origin, X-Requested-Width, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Accedd-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
-})
-
+});
 
 app.use("/products", producRoutes);
 app.use("/orders", ordersRoutes);
+app.use("/user", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
